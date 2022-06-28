68cb80650b570e626d4770a4bb936ea79262e03f
Andrew Smirnov <andrei.smirnov@crplab.ru>
2022-06-28 12:14:53 +0300

    fix validation

--- src/dictionaries/validation/dictionary.ts
+++ src/dictionaries/validation/dictionary.ts
@@ -1,13 +1,5 @@
 export const DICTIONARY = {
-  REQUIRED: 'Поле обязательно для ввода',
-  LARGE_FILE: 'Размер файла слишком большой',
-  REQUIRED_FILE: 'Файл обязателен',
-  INCORRECT_LOGIN_OR_PASSWORD: 'Неверный логин или пароль',
-  SERVER_ERROR: 'Ошибка сервера',
-  SESSION_ERROR: 'Конец сессии, авторизуйтесь заново',
-  SUCCESS: 'Успех',
-  UNACCEPTABLE_FILE_EXTENSION: 'Недопустимый формат файла',
-  NOT_RIGHT_FILE_EXTENSION: 'Неверный формат файла',
-  NO_PERMISSIONS: 'Нет доступа',
-  NOT_FOUND: 'Не найдено'
+  SERVER_ERROR: 'Server error',
+  SUCCESS: 'Success',
+  NOT_FOUND: 'Not found'
 };
